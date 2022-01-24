a7aff69ebcda147c6d7e3cda935ec3c7d4c736b4
nitrotap <kartikinpublic@gmail.com>
2022-01-23 21:41:29 -0700

    adding error handing

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -14,11 +14,17 @@ let getUserRepos = function(user) {
     let apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-            console.log(data, user);
-        });
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: GitHub User Not Found");
+            }
+        }).catch(function (e) {
+        alert("Unable to connect to GitHub");
     });
 };
 
@@ -39,6 +45,12 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 let displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
+
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
 
     // clear old content
     repoContainerEl.textContent = "";
